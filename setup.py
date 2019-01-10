76af69a39b50a128eadc9e92815900c741a05bc8
ludel <ludovic.delsol@ynov.com>
2019-01-10 22:04:10 +0100

    Update python version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version='0.0.1',
     author='Ludovic Delsol',
     install_requires=['pandas', 'sklearn', 'requests'],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     include_package_data=True,
     packages=setuptools.find_packages(),
 )
